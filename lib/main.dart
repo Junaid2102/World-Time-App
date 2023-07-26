2bac071ceedb85ffda5f1275a22cd14e93b7ae84
Junaid Alam <102413407+Junaid2102@users.noreply.github.com>
2023-07-26 14:27:42 +0500

    Routing changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
